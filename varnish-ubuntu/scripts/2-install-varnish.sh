b2a35fc9c4f0eb4090dc09a52d69740d5663708f
Mofe Lee <mofe@me.com>
2017-02-22 00:47:02 +0800

    update scripts

--- varnish-ubuntu/scripts/2-install-varnish.sh
+++ varnish-ubuntu/scripts/2-install-varnish.sh
@@ -3,11 +3,5 @@
 echo '====>>>> install varnish'
 
 sudo apt update
-sudo apt-get install apt-transport-https
-
-curl https://repo.varnish-cache.org/GPG-key.txt | sudo apt-key add -
-echo "deb https://repo.varnish-cache.org/debian/ jessie varnish-4.1" \
-  | sudo tee -a /etc/apt/sources.list.d/varnish-cache.list
-sudo apt-get update
 sudo apt-get install varnish -y
 varnishd -V
--- varnish-ubuntu/scripts/3-setting-up-varnish.sh
+++ varnish-ubuntu/scripts/3-setting-up-varnish.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo '====>>> setting up varnish'
+sudo systemctl stop varnish
 
 sudo sed -i 's/\.host = "127.0.0.1"/\.host = "www.varnish-cache.org"/' /etc/varnish/default.vcl
 sudo sed -i 's/\.port = "8080"/\.port = "80"/' /etc/varnish/default.vcl
