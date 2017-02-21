a34d10f193e1083682f5f5cec81d061527fbdfe7
Mofe Lee <mofe@me.com>
2017-02-21 17:35:46 +0800

    add varnishadm tool

--- varnish-ubuntu/scripts/3-setting-up-varnish.sh
+++ varnish-ubuntu/scripts/3-setting-up-varnish.sh
@@ -5,7 +5,7 @@ echo '====>>> setting up varnish'
 sudo sed -i 's/\.host = "127.0.0.1"/\.host = "www.varnish-cache.org"/' /etc/varnish/default.vcl
 sudo sed -i 's/\.port = "8080"/\.port = "80"/' /etc/varnish/default.vcl
 sudo mkdir -p /etc/systemd/system/varnish.service.d
-sudo cp -f /vagrant/conf/varnish.service.d/customexec.conf /etc/systemd/system/varnish.service.d/customexec.conf
+sudo cp -f ../conf/varnish.service.d/customexec.conf /etc/systemd/system/varnish.service.d/customexec.conf
 
 sudo systemctl daemon-reload
 sudo systemctl start varnish
